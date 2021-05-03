f08d51fd85b2b825547180de971eed1856e9a653
MenuraDewalegama <61499065+MenuraDewalegama@users.noreply.github.com>
2021-05-03 13:53:56 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const Koa = require('koa');
+const dotenv = require('dotenv').config();
+
+const app = new Koa();
+const PORT = process.env.PORT;
+
+app.use(ctx => {
+    ctx.body = 'Hello';
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+});
