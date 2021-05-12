fa2545d61aa4bf6c08c54735214d2eab903e1c6f
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-12 17:13:13 +0530

    Config basic functionality in user.api.js

--- e-commerce-app-backend/api/user.api.js
+++ e-commerce-app-backend/api/user.api.js
@@ -3,41 +3,63 @@
 @date : 12/05/2021
 */
 
+const userDAO = require('../dal/user.dao');
+
 /* User API */
 
 /** get all users. */
 const getAllUsers = () => {
-    return new Promise((resolve, reject) => {
-
+    return new Promise(async (resolve, reject) => {
+        try {
+            resolve(await userDAO.getAllUsers());
+        } catch (error) {
+            reject(error);
+        }
     });
 };
 
 /** get user by given userID. */
 const getUser = (userID) => {
-    return new Promise((resolve, reject) => {
-
+    return new Promise(async (resolve, reject) => {
+        try {
+            resolve(await userDAO.getUser(userID));
+        } catch (error) {
+            reject(error);
+        }
     });
 };
 
 
 /** add a new user. */
 const addUser = (user) => {
-    return new Promise((resolve, reject) => {
-
+    return new Promise(async (resolve, reject) => {
+        try {
+            resolve(await userDAO.getUser(user));
+        } catch (error) {
+            reject(error);
+        }
     });
 };
 
 /** update a user. */
 const updateUser = (userID, userData) => {
-    return new Promise((resolve, reject) => {
-
+    return new Promise(async (resolve, reject) => {
+        try {
+            resolve(await userDAO.updateUser(userID, userData));
+        } catch (error) {
+            reject(error);
+        }
     });
 };
 
 /** delete a new user. */
 const deleteUser = (userID) => {
-    return new Promise((resolve, reject) => {
-
+    return new Promise(async (resolve, reject) => {
+        try {
+            resolve(await userDAO.updateUser(userID));
+        } catch (error) {
+            reject(error);
+        }
     });
 };
 
