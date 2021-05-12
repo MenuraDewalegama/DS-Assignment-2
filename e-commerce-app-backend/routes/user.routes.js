6c8efb329b6f93f3823b18d1df4420469d1b16f4
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-12 15:50:31 +0530

    Add basic routes to the user.routes.js and link with the index.js

--- e-commerce-app-backend/routes/user.routes.js
+++ e-commerce-app-backend/routes/user.routes.js
@@ -2,3 +2,40 @@
 @author : Dhanusha Perera
 @date : 12/05/2021
 */
+
+const Router = require('@koa/router');
+const path = require('path');
+const fs = require('fs');
+const mimeTypes = require('mime-types');
+
+const router = new Router({
+    prefix: "/users"
+});
+
+/** get all users. */
+router.get('/', async (ctx) => {
+    console.log('get method called');
+});
+
+/** get users by ID. */
+router.get('/:id', async (ctx) => {
+    console.log('get with id called');
+});
+
+/** add a user. */
+router.post('/', async (ctx) => {
+    console.log('post method called');
+});
+
+
+/** update a user. */
+router.put('/:id', async (ctx) => {
+    console.log('put method called');
+});
+
+/** delete a user by ID. */
+router.delete('/:id', async (ctx) => {
+    console.log('delete method called');
+});
+
+module.exports = router;
