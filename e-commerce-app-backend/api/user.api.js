40d78287661b4539aa3bdfa1d69fe11919d619f3
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-12 16:03:44 +0530

    Implement the basic skeleton of user.api.js

--- e-commerce-app-backend/api/user.api.js
+++ e-commerce-app-backend/api/user.api.js
@@ -2,3 +2,50 @@
 @author : Dhanusha Perera
 @date : 12/05/2021
 */
+
+/* User API */
+
+/** get all users. */
+const getAllUsers = () => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/** get user by given userID. */
+const getUser = (userID) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+
+/** add a new user. */
+const addUser = (user) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/** update a user. */
+const updateUser = (userID, userData) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/** delete a new user. */
+const deleteUser = (userID) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+
+module.exports = {
+    getAllUsers,
+    getUser,
+    addUser,
+    updateUser,
+    deleteUser
+}
