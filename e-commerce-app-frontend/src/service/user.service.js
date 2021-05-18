69c606ecc4a79f563e6b3f2ebc0fc89511b31ff8
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-19 02:01:05 +0530

    Config getUserByID function(not yet tested).

--- e-commerce-app-frontend/src/service/user.service.js
+++ e-commerce-app-frontend/src/service/user.service.js
@@ -2,3 +2,25 @@
 @author : Dhanusha Perera
 @date : 18/05/2021
 */
+
+const axios = require('axios');
+
+/** Get user by userID by calling backend services. */
+const getUserByID = (userID) => {
+    return new Promise((resolve, reject) => {
+        axios.get(`${process.env.ECOMMERCE_BACKEND_API_URL}/users`).then(response => {
+            if (response.status === 200) {
+                console.error(response.data);
+                resolve(response.data);
+            }
+        }).catch(error => {
+            console.error(error);
+            reject(error);
+        });
+    });
+};
+
+
+module.exports = {
+    getUserByID,
+}
