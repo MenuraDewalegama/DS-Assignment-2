0b9655ceaea652d33ed96d02eabbb6768f492ff7
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-18 14:48:51 +0530

    Config saveProduct function(not tested yet).

--- e-commerce-app-frontend/src/service/product.service.js
+++ e-commerce-app-frontend/src/service/product.service.js
@@ -30,8 +30,55 @@ const getProductByID = (productID) => {
     });
 };
 
+/** Save a new product by calling backend services. */
+const saveProduct = (product) => {
+    return new Promise((resolve, reject) => {
+        const formData = new FormData();
+        formData.append('name', product.name);
+        formData.append('description', product.description);
+        formData.append('unitPrice', product.unitPrice);
+        formData.append('handOnQuantity', product.handOnQuantity);
+
+        /* append the image to the formData, if only client did upload any image. */
+        if (product.hasOwnProperty('productImage') && product.productImage) {
+            formData.append('productImage', product.productImage);
+        }
+
+        const result = axios.post(`${process.env.ECOMMERCE_BACKEND_API_URL}products`, {
+            data: formData
+        });
+
+        result.then(response => {
+            console.log('result.then called!');
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.statusText);
+            console.log(response.headers);
+            console.log(response.config);
+            resolve(response);
+        }).catch(error => {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            reject(error);
+        });
+    });
+};
 
 module.exports = {
     getAllProducts,
     getProductByID,
+    saveProduct
 };
