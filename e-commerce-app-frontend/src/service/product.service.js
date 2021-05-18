bd6e678df0061c424fa3238f8fef4e334ec78427
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-18 15:27:27 +0530

    Config updateProduct function(not yet tested).

--- e-commerce-app-frontend/src/service/product.service.js
+++ e-commerce-app-frontend/src/service/product.service.js
@@ -5,7 +5,8 @@
 
 const axios = require('axios');
 
-/** Get all products by calling backend services. */
+/** Get all products by calling backend services.
+ * @return Promise promise a result. */
 const getAllProducts = () => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -18,7 +19,9 @@ const getAllProducts = () => {
 };
 
 
-/** Get product by productID by calling backend services. */
+/** Get product by productID by calling backend services.
+ * @param productID ID of the product that is being retrieved.
+ * @return Promise promise a result. */
 const getProductByID = (productID) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -30,20 +33,14 @@ const getProductByID = (productID) => {
     });
 };
 
-/** Save a new product by calling backend services. */
+/** Save a new product by calling backend services.
+ * @param product product object with and new values but the ID .
+ * @returns Promise promise a result. */
 const saveProduct = (product) => {
     return new Promise((resolve, reject) => {
-        const formData = new FormData();
-        formData.append('name', product.name);
-        formData.append('description', product.description);
-        formData.append('unitPrice', product.unitPrice);
-        formData.append('handOnQuantity', product.handOnQuantity);
+        const formData = configureFormDataObject(true, product);
 
-        /* append the image to the formData, if only client did upload any image. */
-        if (product.hasOwnProperty('productImage') && product.productImage) {
-            formData.append('productImage', product.productImage);
-        }
-
+        /* send a post request to the backend using axios. */
         const result = axios.post(`${process.env.ECOMMERCE_BACKEND_API_URL}products`, {
             data: formData
         });
@@ -77,8 +74,85 @@ const saveProduct = (product) => {
     });
 };
 
+/** Update a existing product by calling backend services.
+ * @param product product object with the ID and new values.
+ * @returns Promise promise a result. */
+const updateProduct = (product) => {
+    return new Promise((resolve, reject) => {
+        const formData = configureFormDataObject(false, product);
+
+        /* send a put request to the backend using axios. */
+        const result = axios.put(`${process.env.ECOMMERCE_BACKEND_API_URL}products/${product?.id}`, {
+            data: formData
+        });
+
+        result.then(response => {
+            console.log('result.then called!');
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.statusText);
+            console.log(response.headers);
+            console.log(response.config);
+            resolve(response);
+        }).catch(error => {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            reject(error);
+        });
+    });
+};
+
+
+/** Creates a new FormData object and append product object properties accordingly.
+ * @param isAdding boolean, if true formData object will be ready for adding a new product.
+ * otherwise, formData object will be ready for update a product record.
+ * @param product product object that is used to be saved or updated.
+ * @return FormData configured formData object. */
+const configureFormDataObject = (isAdding, product) => {
+    const formData = new FormData();
+    formData.append('name', product.name);
+    formData.append('description', product.description);
+    formData.append('unitPrice', product.unitPrice);
+    formData.append('handOnQuantity', product.handOnQuantity);
+
+    if (isAdding && product.hasOwnProperty('productImage') && product.productImage) {
+        /* append the image to the formData, if only client did upload any image. */
+        formData.append('productImage', product.productImage);
+    }
+
+    if (!isAdding) {
+        /* ready the formData object for update operation. */
+        if (product.hasOwnProperty('productImage') && product.productImage) {
+            /* append the image to the formData, if only client did upload any image. */
+            formData.append('productImage', product.productImage);
+        } else {
+            /* append the imagePath to the formData, if only client did not upload any image
+            or if in case client wants to remove the existing image. */
+            if (product.hasOwnProperty('imagePath') && product.imagePath) {
+                formData.append('imagePath', product.imagePath);
+            }
+        }
+    }
+
+    return formData;
+};
+
 module.exports = {
     getAllProducts,
     getProductByID,
-    saveProduct
+    saveProduct,
+    updateProduct,
 };
