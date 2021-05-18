fe9e5b99457a2cfd61f742cad5183999a2cb562a
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-18 13:26:55 +0530

    Config getAllProducts function.

--- e-commerce-app-frontend/src/service/product.service.js
+++ e-commerce-app-frontend/src/service/product.service.js
@@ -2,3 +2,21 @@
 @author : Dhanusha Perera
 @date : 18/05/2021
 */
+
+const axios = require('axios');
+
+/** Get all products by calling backend services. */
+const getAllProducts = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const result = await axios.get(`${process.env.ECOMMERCE_BACKEND_API_URL}products`);
+            resolve(result);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+module.exports = {
+    getAllProducts
+};
