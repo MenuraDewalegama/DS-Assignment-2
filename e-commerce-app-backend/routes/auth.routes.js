c9107918b13a5d0d1a3b1509da8c5180cf5689f4
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-19 13:29:55 +0530

    Add routing post method.

--- e-commerce-app-backend/routes/auth.routes.js
+++ e-commerce-app-backend/routes/auth.routes.js
@@ -2,3 +2,13 @@
 @author : Dhanusha Perera
 @date : 19/05/2021
 */
+const Router = require('@koa/router');
+
+const router = new Router({
+    prefix: '/auth'
+});
+
+/** Authenticate the user.*/
+router.post('/', ctx => {
+    console.log(ctx);
+});
