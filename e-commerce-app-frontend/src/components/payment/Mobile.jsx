3d688135f4c7018e37d56eca024bbd3f5d13c11f
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-20 19:07:25 +0530

    Reformat code

--- e-commerce-app-frontend/src/components/payment/Mobile.jsx
+++ e-commerce-app-frontend/src/components/payment/Mobile.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import * as ReactBootStrap from 'react-bootstrap';
+import {Button, Form} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-
 
 export default class Mobile extends React.Component {
 
@@ -13,28 +12,25 @@ export default class Mobile extends React.Component {
 
                 <br/>
                 <br/>
-                <ReactBootStrap.Form>
-                    <ReactBootStrap.Form.Group controlId="formBasicEmail">
-                        <ReactBootStrap.Form.Label>Amount</ReactBootStrap.Form.Label>
-                        <ReactBootStrap.Form.Control type="name" placeholder="Username"/>
-                    </ReactBootStrap.Form.Group>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Amount</Form.Label>
+                        <Form.Control type="name" placeholder="Username"/>
+                    </Form.Group>
 
-                    <ReactBootStrap.Form.Group controlId="formBasicPassword">
-                        <ReactBootStrap.Form.Label>Mobile number</ReactBootStrap.Form.Label>
-                        <ReactBootStrap.Form.Control type="name" placeholder="address"/>
-                    </ReactBootStrap.Form.Group>
-
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Mobile number</Form.Label>
+                        <Form.Control type="name" placeholder="address"/>
+                    </Form.Group>
 
 
                     <br/>
-                    <ReactBootStrap.Button variant="primary">
+                    <Button variant="primary">
                         {/* // TODO: if registration successful, then redirect to root URL (/product). */}
                         <Link to="/delivery" style={{textDecoration: 'none', color: 'white'}}>Confirm</Link>
-                    </ReactBootStrap.Button>
-                </ReactBootStrap.Form>
+                    </Button>
+                </Form>
                 <br/>
-
-
             </div>
         );
     }
