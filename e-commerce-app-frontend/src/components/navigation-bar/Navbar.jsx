2799efe5ba35f259d3f61a2848e0327ed83d5ca8
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-20 18:35:42 +0530

    Change index.html global font size to 12px

--- e-commerce-app-frontend/src/components/navigation-bar/Navbar.jsx
+++ e-commerce-app-frontend/src/components/navigation-bar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import {Badge, Button, Nav, Navbar} from 'react-bootstrap';
 import {Link, Route, Switch} from 'react-router-dom';
+import {Cart2, House} from 'react-bootstrap-icons';
 import './Nabar.css';
 
 /* components. */
 import Login from '../login/Login';
 import Register from '../register/Register';
 import Cart from '../cart/Cart';
-import {Cart2, House} from 'react-bootstrap-icons';
 import Delivery from '../delivery/Delivery';
 import Credit from '../payment/Credit';
 import Mobile from '../payment/Mobile';
@@ -36,25 +36,25 @@ export default class ButtonAppBar extends Component {
                         </Nav>
 
                         {     // if user logged in
-                            (true) ?
+                            (false) ?
                                 <Nav>
-                                    <h4 className="user_name">Hi User! </h4>
+                                    <h5 className="user_name">Hi User!</h5>
                                     <Link style={{margin: 'auto'}} to="/cart" className="nav-link">
-                                        <Badge variant="secondary">
-                                            <Cart2 color="white" size={40}/>
-                                        </Badge>
+                                        <Cart2 className="navbar_icon"/>
                                     </Link>
 
-                                    <Button variant="danger" style={{height: '30%', margin: 'auto'}}>
-                                        <Link to="/login" className="nav-link"> Logout</Link>
-                                    </Button>
+
+                                    <Link to="/login" className="nav-link">
+                                        <Button variant="danger">Logout</Button>
+                                    </Link>
+
                                 </Nav>
 
                                 //if user is not logged in
 
                                 : <Nav>
-                                    <Link to="/login" className="nav-link"> Login </Link>
-                                    <Link to="/register" className="nav-link"> Register </Link>
+                                    <Link to="/login" className="nav-link navigation_property">Login</Link>
+                                    <Link to="/register" className="nav-link navigation_property">Register</Link>
                                 </Nav>
                         }
 
