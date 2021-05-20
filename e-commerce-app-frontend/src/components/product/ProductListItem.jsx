ffec02fec31494fc46fbddee852a0beab8decd47
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-20 20:30:47 +0530

    Retouch ProductListItem component.

--- e-commerce-app-frontend/src/components/product/ProductListItem.jsx
+++ e-commerce-app-frontend/src/components/product/ProductListItem.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {Button, Card, Col, Row} from 'react-bootstrap';
+/*icons*/
+import {CartPlus, PencilSquare} from 'react-bootstrap-icons';
 import {Link, useHistory} from 'react-router-dom';
 
 /* TODO: this will be removed. */
 export const cartList = [];
+
+const fontStyle = {
+    fontSize: 'medium'
+};
 
 export default function ProductListItem(props) {
 
@@ -24,31 +30,31 @@ export default function ProductListItem(props) {
             <br/>
             <Row>
                 <Col>
-                    <Card style={{width: '18rem'}}>
+                    <Card style={{width: '20rem'}}>
                         <Card.Img variant="top" src="https://via.placeholder.com/300"/>
                         <Card.Body>
                             <Card.Title>Name: {product.name}</Card.Title>
-                            <Card.Text>Description: {product.description}</Card.Text>
-                            <Card.Text>Price: {product.price}</Card.Text>
-                            <Card.Text>Qty: {product.quantity}</Card.Text>
+                            <Card.Text style={fontStyle}>Description: {product.description}</Card.Text>
+                            <Card.Text>Unit Price: {product.price}</Card.Text>
+                            <Card.Text>Hand On Quantity: {product.quantity}</Card.Text>
 
                             <Row>
 
                                 {   // product edit button
-                                    (true) ? <Button style={{margin: 'auto'}} variant="primary"
+                                    (true) ? <Button style={{margin: 'auto', padding: '0.4rem 1rem'}} variant="primary"
                                                      onClick={() => {
                                                          selectProduct(product);
                                                          redirectToEdit();
-                                                     }}>Edit
-                                        Item</Button> : ''}
+                                                     }} title="Edit Product">
+                                        <PencilSquare style={{fontSize: '1.6rem'}}/>
+                                    </Button> : ''}
 
                                 <Link to="/cart" style={{color: 'white', margin: 'auto'}}>
-                                    <Button variant="primary"
+                                    <Button style={{margin: 'auto', padding: '0.4rem 1rem'}} variant="primary"
                                             onClick={() => {
                                                 selectProduct(product);
                                                 onClickAddToCart(product);
-                                            }}
-                                    >Add to cart
+                                            }} title="Add to Cart"><CartPlus style={{fontSize: '1.6rem'}}/>
                                     </Button>
                                 </Link>
                             </Row>
