22c79e3969b2bf81f5345b0db422ac3eac3540e5
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-20 15:29:53 +0530

    Config product context and Implement getAllProducts function.

--- e-commerce-app-frontend/src/context/product.context.js
+++ e-commerce-app-frontend/src/context/product.context.js
@@ -2,3 +2,66 @@
 @author : Dhanusha Perera
 @date : 20/05/2021
 */
+import React, {Component} from 'react';
+import ProductService from '../service/product.service';
+
+/* create product context with default values. */
+const ProductContext = React.createContext({
+    products: [],
+    getAllProducts: () => {
+    },
+    getProductByID: () => {
+    },
+    addProduct: (product) => {
+    },
+    updateProduct: (product) => {
+    },
+    deleteProduct: (productID) => {
+    },
+});
+
+class ProductProvider extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
+    /** Get all the products by calling backend.
+     * @return Promise with a result. If success, then resolve the product.
+     * otherwise, reject the error(errorRespond) */
+    getAllProducts() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await ProductService.getAllProducts();
+                if (response.status === 200) {
+                    this.setState({
+                        products: JSON.parse(response.data)
+                    });
+                    resolve(this.state.products);
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+
+    render() {
+        return (
+            <ProductContext.Provider value={{
+                products: this.state.products,
+                getAllProducts: this.getAllProducts(),
+                getProductByID: this.getProductByID,
+                addProduct: this.addProduct(),
+                updateProduct: this.updateProduct(),
+                deleteProduct: this.deleteProduct(),
+            }
+            }>
+                {this.props.children}
+            </ProductContext.Provider>
+        );
+    }
+}
