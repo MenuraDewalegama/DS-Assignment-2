0f33abf58366de783f85a81f752ac2d578ec81d4
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-22 01:56:22 +0530

    Surround the <App/> component with Product provider.

--- e-commerce-app-frontend/index.jsx
+++ e-commerce-app-frontend/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 import {UserProvider} from './src/context/user.context';
+import {ProductProvider} from './src/context/product.context';
 
 render(
-    <UserProvider>
+    <ProductProvider><UserProvider>
         <App/>
-    </UserProvider>
+    </UserProvider></ProductProvider>
     , document.getElementById('app'));
 
