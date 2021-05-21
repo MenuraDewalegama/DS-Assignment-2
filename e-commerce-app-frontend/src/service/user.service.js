cfb4612f36d0a7409ce506acb967271af9b201c1
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-22 01:19:59 +0530

    Config jwt functionality in frontend.

--- e-commerce-app-frontend/src/service/user.service.js
+++ e-commerce-app-frontend/src/service/user.service.js
@@ -5,12 +5,12 @@
 
 const axios = require('axios');
 import sha256 from 'crypto-js/sha256';
-import {printErrorResponse} from './error.service';
 
 /** Call the auth API and get a token using userID and password.
  * @param userID user's ID
  * @param password password of the user.
- * @return Promise with result. if auth success then resolves true, otherwise reject error. */
+ *
+ * @return Promise with result. if auth success then resolves JWT, otherwise reject error. */
 const authenticate = ({userID, password}) => {
     const body = {
         username: userID,
@@ -23,28 +23,15 @@ const authenticate = ({userID, password}) => {
             responseType: 'text'
         }).then(response => {
             /* JWT should be available here. */
-            // TODO: JWT should be handled here
             if (response.status === 200) {
                 const jwtToken = response.data;
-                console.log(response);
-                console.log(`received JWT: ${jwtToken}`);
-                /* save encoded JWT Token. */
-                sessionStorage.setItem(sha256(process.env.JWT_TOKEN_NAME), jwtToken);
-
-                /* get user information from the backend.
-                * save the relevant information. */
-                getUserByID(userID).then(authUser => {
-                    console.log(`Authenticated user details: ${authUser}`);
-                    /* store current authenticated user ID in session storage. */
-                    sessionStorage.setItem(sha256(process.env.AUTHENTICATED_USER_ID),
-                        btoa(JSON.stringify(authUser.id)));
-                    /* store current authenticated user name in session storage. */
-                    // sessionStorage.setItem(sha256(process.env.AUTHENTICATED_USER_NAME),
-                    //     btoa(JSON.stringify(authUser.name)));
-                    resolve(true);
-                }).catch((error) => {
-                    printErrorResponse(error);
-                });
+                /* set bearer token globally for axios. */
+                axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
+                // setBearerTokenForAxiosGlobally(jwtToken);
+                /* save encoded JWT Token in session storage. */
+                setValueOnSessionStorage(sha256(process.env.JWT_TOKEN_NAME), jwtToken);
+                // setValueOnSessionStorage(sha256(process.env.AUTHENTICATED_USER_ID), btoa(userID));
+                resolve(jwtToken);
             }
         }).catch(error => {
             reject(error);
@@ -53,8 +40,32 @@ const authenticate = ({userID, password}) => {
 };
 
 
+/** Set key-value pair in the session storage.
+ * @param key unique key to identify the attached value.
+ * @param value the value to be saved. */
+const setValueOnSessionStorage = (key, value) => {
+    /* save encoded JWT Token. */
+    sessionStorage.setItem(key, value);
+};
+
+
+/** Get the value by the key in sessionStorage.
+ * @param key unique key to identify the attached value.
+ * @return String value for that given key. */
+const getValueFromSessionStorage = (key) => {
+    return sessionStorage.getItem(key);
+};
+
+
+/** Set axios global config, the Bearer token. */
+// const setBearerTokenForAxiosGlobally = (jwtToken) => {
+//     axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
+// };
+
+
 /** Get user by userID by calling backend services.
  * @param userID ID of the user to be retrieved.
+ *
  * @return Promise with result. if success then resolves the user data, otherwise reject the error. */
 const getUserByID = (userID) => {
     return new Promise((resolve, reject) => {
@@ -64,13 +75,16 @@ const getUserByID = (userID) => {
                 resolve(response.data);
             }
         }).catch(error => {
+            console.error('getUserByID', error);
             reject(error);
         });
     });
 };
 
+
 /** User registration(Add a new user) by calling backend services.
  * @param user user object containing values.
+ *
  * @return Promise a promise with a result. If successful then, resolve the response,
  * otherwise reject the error. */
 const addUser = (user) => {
