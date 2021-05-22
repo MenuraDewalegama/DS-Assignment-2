129036e1ff84c62bafab6011d300038ed924de8c
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-22 23:26:14 +0530

    Fix nav issue.

--- e-commerce-app-frontend/src/components/navigation-bar/Navbar.jsx
+++ e-commerce-app-frontend/src/components/navigation-bar/Navbar.jsx
@@ -40,10 +40,9 @@ export default class NavigationBar extends Component {
                         </Nav>
 
                         {     // if user logged in
-                            (true) ? //todo: change this to 'currentuser'
+                            (currentUser) ?
                                 <Nav>
-                                    <h5 className="user_name">Hi User {currentUser?.name}</h5>
-                                   
+                                    <h5 className="user_name">Hi {currentUser?.name}</h5>
                                     <Link style={{margin: 'auto'}} to="/cart" className="nav-link">
                                         <Cart2 className="navbar_icon"/>
                                     </Link>
@@ -51,10 +50,9 @@ export default class NavigationBar extends Component {
                                     <Button className="nav-link" variant="danger"
                                             onClick={() => this.performLogOut(logOutUser)}>Logout</Button>
                                 </Nav>
+
                                 //if user is not logged in
                                 : <Nav>
-                                  
-
                                     <Link to="/login" className="nav-link navigation_property">Login</Link>
                                     <Link to="/register" className="nav-link navigation_property">Register</Link>
                                 </Nav>
