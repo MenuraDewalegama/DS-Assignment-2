b0a93bc5c0852ae4012a897b1efc083ba4bf5a29
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-23 00:48:43 +0530

    Fix issue in add product.

--- e-commerce-app-frontend/src/components/product/ProductsHolder.jsx
+++ e-commerce-app-frontend/src/components/product/ProductsHolder.jsx
@@ -1,27 +1,16 @@
-import React from "react";
-import Products from "./Products";
-import { Route, Switch, Redirect } from "react-router-dom";
-import AddEditProduct from "./AddEditProduct";
-import { ProductContext } from "../../context/product.context";
-import sha256 from "crypto-js/sha256";
+import React from 'react';
+import Products from './Products';
+import {Redirect, Route, Switch} from 'react-router-dom';
+import AddEditProduct from './AddEditProduct';
+import {ProductContext} from '../../context/product.context';
+import sha256 from 'crypto-js/sha256';
 
 export default class ProductsHolder extends React.Component {
+
   static contextType = ProductContext;
 
   constructor(props) {
     super(props);
-  }
-
-  /** this method is also temporary method. this will be removed as soon as possible,
-     when context API is implemented. */
-  addNewProduct({ name, description, price, quantity }) {
-    products.push({
-      id: products.length + 1,
-      name,
-      description,
-      price,
-      quantity,
-    });
   }
 
   /** temporary method to add a new product. */
@@ -36,69 +25,25 @@ export default class ProductsHolder extends React.Component {
 
   render() {
     // console.log('product holder works', this.context);
-    const isAdmin =
-      atob(
-        sessionStorage.getItem(sha256(process.env.AUTHENTICATED_USER_TYPE))
-      ) === "ADMIN";
-    const { cartList } = this.props;
-
+    const isAdmin = (atob(sessionStorage.getItem(sha256(process.env.AUTHENTICATED_USER_TYPE))) === 'ADMIN');
     return (
-      <div>
-        <Switch>
-          <Route exact path="/products">
-            {/* products component. */}
-            <Products
-              products={this.context?.products ? this.context?.products : []}
-            />
-          </Route>
-          <Route
-            exact
-            path="/products/add"
-            render={(props) => (
-              <AddEditProduct {...props} saveOrUpdate={this.addProduct} />
-            )}
-          />
-          <Route
-            exact
-            path="/products/:productID/edit"
-            render={(props) => (
-              <AddEditProduct {...props} saveOrUpdate={this.updateProduct} />
-            )}
-          />
-          <Products
-            products={this.context?.products ? this.context?.products : []}
-          />
-          {/* Protected routes - if User Type == 'ADMIN' then only client can navigate to these routes. */}
-          <Route
-            exact
-            path="/products/add"
-            render={(props) =>
-              isAdmin ? (
-                <AddEditProduct
-                  {...props}
-                  saveOrUpdate={this.addProduct.bind(this)}
-                />
-              ) : (
-                <Redirect to="/" />
-              )
-            }
-          />
-          <Route
-            exact
-            path="/products/:productID/edit"
-            render={(props) =>
-              isAdmin ? (
-                <AddEditProduct
-                  {...props}
-                  saveOrUpdate={this.updateProduct.bind(this)}
-                />
-              ) : (
-                <Redirect to="/" />
-              )
-            }
-          />
-        </Switch>
-      </div>
+        <div>
+          <Switch>
+            <Route exact path="/products">
+              {/* products component. */}
+              <Products products={(this.context?.products) ? this.context?.products : []}/>
+            </Route>
+            {/* Protected routes - if User Type == 'ADMIN' then only client can navigate to these routes. */}
+            <Route exact path="/products/add"
+                   render={(props) => ((isAdmin) ?
+                       <AddEditProduct {...props} saveOrUpdate={this.addProduct.bind(this)}/> :
+                       <Redirect to="/"/>)}/>
+            <Route exact path="/products/:productID/edit"
+                   render={(props) => ((isAdmin) ?
+                       <AddEditProduct {...props} saveOrUpdate={this.updateProduct.bind(this)}/> :
+                       <Redirect to="/"/>)}/>
+          </Switch>
+        </div>
     );
   }
 }
