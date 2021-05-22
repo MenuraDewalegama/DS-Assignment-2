24e4abdd0d888afa20e5415791c3e31c5d9b1a1e
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-23 03:08:24 +0530

    Change user registration form fields.

--- e-commerce-app-frontend/src/components/register/Register.jsx
+++ e-commerce-app-frontend/src/components/register/Register.jsx
@@ -5,6 +5,29 @@ import {Link} from 'react-router-dom';
 
 export default class Register extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: null,
+            contactNo: null,
+            password: null,
+            type: null
+        };
+    }
+
+    /** Register the user. */
+    onSubmit() {
+        console.log('register called!');
+        console.log(this.state);
+    }
+
+    /* keep track of changes of the form field values. */
+    onChange(event) {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+        console.log(name, value);
+    }
+
     render() {
         return (
             <div className="container-sm">
@@ -14,26 +37,45 @@ export default class Register extends React.Component {
                 <br/>
                 <br/>
                 <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="name" placeholder="Username"/>
+                    <Form.Group controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control name="name"
+                                      type="name"
+                                      placeholder="Name"
+                                      onChange={(event) => this.onChange(event)}
+                        />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password"/>
+                        <Form.Control name="password"
+                                      type="password"
+                                      placeholder="Password"
+                                      onChange={(event) => this.onChange(event)}
+                        />
                     </Form.Group>
 
-                    <Form.Group controlId="formGridState">
+                    <Form.Group controlId="formBasicContactNo">
                         <Form.Label>Content No</Form.Label>
-                        <Form.Control type="name" placeholder="Username"/>
+                        <Form.Control type="name"
+                                      placeholder="Contact No"
+                                      onChange={(event) => this.onChange(event)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicDelivery">
+                        <Form.Label>User Type</Form.Label>
+                        <Form.Control name="type" as="select" defaultValue="DHL"
+                                      onChange={(event) => this.onChange(event)}>
+                            <option value="USER">USER</option>
+                            <option value="ADMIN">ADMIN</option>
+                        </Form.Control>
                     </Form.Group>
 
                     <br/>
-                    <Button variant="primary">
-                        {/* // TODO: if registration successful, then redirect to root URL (/product). */}
-                        <Link to="/" style={{textDecoration: 'none', color: 'white'}}>Register</Link>
-                    </Button>
+                    <Button variant="primary"
+                            onClick={this.onSubmit.bind(this)}
+                    >Register</Button>
                 </Form>
                 <br/>
 
